916fffb84d1d0e5ed03313b4a979e964513c1717
thanhbn <thanhbn87@gmail.com>
2019-09-22 10:18:00 +0700

    add tf files

---
+++ main.tf
@@ -0,0 +1,126 @@
+provider "aws" {
+  region  = "${var.aws_region}"
+  profile = "${var.aws_profile}"
+}
+
+terraform {
+  backend "s3" {}
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config {
+    bucket         = "${var.tfstate_bucket}"
+    key            = "${var.tfstate_key_vpc}"
+    region         = "${var.tfstate_region}"
+    profile        = "${var.tfstate_profile}"
+    role_arn       = "${var.tfstate_arn}"
+  }
+}
+
+data "aws_security_groups" "elb" {
+  tags = "${var.source_elb_sg_tags}"
+}
+
+resource aws_s3_bucket "log" {
+  count  = "${var.logging_enabled && var.log_bucket_name == "" ? 1 : 0 }"
+  bucket = "${local.name}-log"
+  acl    = "private"
+  tags   = "${merge(local.common_tags, var.tags)}"
+}
+
+data "aws_acm_certificate" "cert" {
+  count       = "${var.https_listeners_count > 0 ? 1 : 0}"
+  domain      = "${var.cert_domain}"
+  statuses    = ["ISSUED"]
+  most_recent = true
+}
+
+locals {
+  name        = "${var.namespace == "" ? "" : "${lower(var.namespace)}-"}${lower(var.project_env_short)}-${lower(var.name)}"
+  common_tags = {
+         Env  = "${var.project_env}"
+         Name = "${local.name}"
+  }
+
+  dynamic_subnets     = [ "${split(",", var.load_balancer_is_internal ? join(",", data.terraform_remote_state.vpc.private_subnets) : join(",", data.terraform_remote_state.vpc.public_subnets))}" ]
+  subnets             = [ "${split(",", length(var.subnets) > 0 ? join(",", var.subnets) : join(",", local.dynamic_subnets) )}" ]
+  security_groups     = "${data.aws_security_groups.elb.*.id}"
+  log_bucket_name     = "${var.logging_enabled && var.log_bucket_name == "" ? "${local.name}-log" : var.log_bucket_name }"
+  log_location_prefix = "${var.log_location_prefix == "" ? local.name : var.log_location_prefix }"
+  https_listeners     = "${var.https_listeners_count > 0 ? list(map("certificate_arn", "${data.aws_acm_certificate.cert.arn}", "port", "${var.https_port}")) : list(map()) }"
+  http_tcp_listeners  = "${list(map("port", "${var.http_port}", "protocol", "HTTP"))}"
+}
+
+resource "null_resource" "target_groups" {
+  count = "${length(var.target_group_names)}"
+
+  triggers {
+    name                             = "${element(var.target_group_names,count.index)}"
+    backend_protocol                 = "${var.backend_protocol}"
+    backend_port                     = "${var.backend_port}"
+    health_check_interval            = "${var.health_check_interval}"
+    health_check_healthy_threshold   = "${var.health_check_healthy_threshold}"
+    health_check_unhealthy_threshold = "${var.health_check_unhealthy_threshold}"
+    health_check_path                = "${var.health_check_path}"
+    health_check_matcher             = "${var.health_check_matcher}"
+    stickiness_enabled               = "{var.stickiness_enabled}"
+  }
+}
+
+////// Application Load balancer
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "3.6.0"
+  tags    = "${merge(var.tags,local.common_tags)}"
+  
+  load_balancer_name       = "${local.name}"
+  security_groups          = "${local.security_groups}"
+  logging_enabled          = "${var.logging_enabled}"
+  log_bucket_name          = "${local.log_bucket_name}"
+  log_location_prefix      = "${local.log_location_prefix}"
+  subnets                  = "${local.subnets}"
+  vpc_id                   = "${data.terraform_remote_state.vpc.vpc_id}"
+  https_listeners          = "${local.https_listeners}"
+  https_listeners_count    = "${var.https_listeners_count}"
+  http_tcp_listeners       = "${local.http_tcp_listeners}"
+  http_tcp_listeners_count = "1"
+  target_groups            = "${null_resource.target_groups.*.triggers}"
+  target_groups_count      = "${length(var.target_group_names)}"
+
+  enable_http2                 = "${var.enable_http2}"
+  ip_address_type              = "${var.ip_address_type}"
+  idle_timeout                 = "${var.idle_timeout}"
+  enable_deletion_protection   = "${var.enable_deletion_protection}"
+  extra_ssl_certs_count        = "${var.extra_ssl_certs_count}"
+  extra_ssl_certs              = "${var.extra_ssl_certs}"
+  listener_ssl_policy_default  = "${var.listener_ssl_policy_default}"
+  load_balancer_is_internal    = "${var.load_balancer_is_internal}"
+  load_balancer_create_timeout = "${var.load_balancer_create_timeout}"
+  load_balancer_delete_timeout = "${var.load_balancer_delete_timeout}"
+  load_balancer_update_timeout = "${var.load_balancer_update_timeout}"
+  enable_cross_zone_load_balancing = "${var.enable_cross_zone_load_balancing}"
+
+}
+
+////// Target Group Attachment
+locals = {
+  num_instance_tags = "${length(keys(var.instance_tags))}"
+}
+
+data "aws_instances" "ec2" {
+  count         = "${local.num_instance_tags > 0 ? 1 : 0}"  
+  instance_tags = "${local.instance_tags}"
+}
+
+locals = {
+  ec2_instances = "${flatten(coalescelist(data.aws_instances.ec2.*.ids,list()))}"
+}
+
+resource "aws_lb_target_group_attachment" "ec2_http" {
+  count            = "${local.num_instance_tags > 0 ? length(local.ec2_instances) : 0}"
+  target_group_arn = "${module.alb.target_group_arns[0]}"
+  target_id        = "${element(local.ec2_instances, count.index)}"
+  port             = "${http_port}"
+}
---
+++ vars.tf
@@ -0,0 +1,127 @@
+variable "project_env" { default = "Production" }
+variable "project_env_short" { default = "prd" }
+
+variable "aws_region" { default = "us-west-2" }
+variable "aws_profile" { default = "default" }
+
+variable "tfstate_bucket" { default = "example-tfstate-bucket" }
+variable "tfstate_region" { default = "us-west-2" }
+variable "tfstate_profile" { default = "default" }
+variable "tfstate_arn" { default = "" }
+variable "tfstate_key_vpc" { default = "demo/vpc/terraform.tfstate" }
+
+variable "source_elb_sg_tags" { default = { Type = "ALB" } }
+variable "instance_tags" { default = { Type = "WebApp" } }
+variable "tags" { default = {} }
+
+variable "domain_name" { default = "example.com" }
+variable "cert_domain" { default = "" }
+
+variable "namespace" { default = "" }
+variable "name" { default = "alb" }
+
+variable "https_port" { default = "443" }
+variable "http_port" { default = "80" }
+variable "target_group_names" { default = ["webapp"] }
+
+variable "backend_protocol" { default = "HTTP" } # HTTP, TCP
+variable "backend_port" { default = "80" }
+variable "health_check_interval" { default = "10" }
+variable "health_check_healthy_threshold" { default = "2" }
+variable "health_check_unhealthy_threshold" { default = "2" }
+variable "health_check_path" { default = "/" }
+variable "health_check_matcher" { default = "200-209" }
+variable "stickiness_enabled" { default = false }
+
+## Vars from the [terraform module](https://github.com/terraform-aws-modules/terraform-aws-alb/blob/v3.6.0/variables.tf)
+variable "create_alb" {
+  description = "Controls if the ALB should be created"
+  default     = true
+}
+
+variable "enable_deletion_protection" {
+  description = "If true, deletion of the load balancer will be disabled via the AWS API. This will prevent Terraform from deleting the load balancer. Defaults to false."
+  default     = false
+}
+
+variable "enable_http2" {
+  description = "Indicates whether HTTP/2 is enabled in application load balancers."
+  default     = true
+}
+
+variable "enable_cross_zone_load_balancing" {
+  description = "Indicates whether cross zone load balancing should be enabled in application load balancers."
+  default     = false
+}
+
+variable "extra_ssl_certs" {
+  description = "A list of maps describing any extra SSL certificates to apply to the HTTPS listeners. Required key/values: certificate_arn, https_listener_index (the index of the listener within https_listeners which the cert applies toward)."
+  type        = "list"
+  default     = []
+}
+
+variable "extra_ssl_certs_count" {
+  description = "A manually provided count/length of the extra_ssl_certs list of maps since the list cannot be computed."
+  default     = 0
+}
+
+variable "https_listeners_count" {
+  description = "A manually provided count/length of the https_listeners list of maps since the list cannot be computed."
+  default     = 1
+}
+
+variable "idle_timeout" {
+  description = "The time in seconds that the connection is allowed to be idle."
+  default     = 60
+}
+
+variable "ip_address_type" {
+  description = "The type of IP addresses used by the subnets for your load balancer. The possible values are ipv4 and dualstack."
+  default     = "ipv4"
+}
+
+variable "listener_ssl_policy_default" {
+  description = "The security policy if using HTTPS externally on the load balancer. [See](https://docs.aws.amazon.com/elasticloadbalancing/latest/classic/elb-security-policy-table.html)."
+  default     = "ELBSecurityPolicy-2016-08"
+}
+
+variable "load_balancer_is_internal" {
+  description = "Boolean determining if the load balancer is internal or externally facing."
+  default     = false
+}
+
+variable "load_balancer_create_timeout" {
+  description = "Timeout value when creating the ALB."
+  default     = "10m"
+}
+
+variable "load_balancer_delete_timeout" {
+  description = "Timeout value when deleting the ALB."
+  default     = "10m"
+}
+
+variable "load_balancer_update_timeout" {
+  description = "Timeout value when updating the ALB."
+  default     = "10m"
+}
+
+variable "logging_enabled" {
+  description = "Controls if the ALB will log requests to S3."
+  default     = true
+}
+
+variable "log_bucket_name" {
+  description = "S3 bucket (externally created) for storing load balancer access logs. Required if logging_enabled is true."
+  default     = ""
+}
+
+variable "log_location_prefix" {
+  description = "S3 prefix within the log_bucket_name under which logs are stored."
+  default     = ""
+}
+
+variable "subnets" {
+  description = "A list of subnets to associate with the load balancer. e.g. ['subnet-1a2b3c4d','subnet-1a2b3c4e','subnet-1a2b3c4f']"
+  type        = "list"
+  default     = []
+}
